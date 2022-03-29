96ae5987d2762c411b3fdcca46c5726a169e6820
Md Foysal Hossain <79256105+cyberteach360@users.noreply.github.com>
2022-03-29 06:21:00 -0700

    Create index.php

---
+++ XSS/index.php
@@ -0,0 +1,68 @@
+<?php
+ $msg='';
+ $flag='';
+
+ $msg=base64_decode($_GET['msg']);
+//echo base64_encode($_GET['msg']);
+//echo base64_decode($_GET['msg']);
+//echo ($_GET['msg']);
+
+if(isset($msg)){
+ if($msg=="<script>alert('flag');</script>"){
+	 $flag='IEEEIUCS-CTF{Easy_XSS_FOR_YOU}';
+  $msg= $msg;
+}
+ else if($msg=='<<SCRIPT>alert("XSS");//<</SCRIPT>'){
+	 $flag='IEEEIUCS-CTF{Easy_XSS_FOR_YOU}';
+  $msg= $msg;
+}
+ else if($msg=='<IMG SRC=x onerror="alert(String.fromCharCode(88,83,83))">'){
+	 $flag='IEEEIUCS-CTF{Easy_XSS_FOR_YOU}';
+  $msg= $msg;
+}
+ else if($msg=="<img/src=`` onerror=this.onerror=confirm(1)"){
+	 $flag='IEEEIUCS-CTF{Easy_XSS_FOR_YOU}';
+  $msg= $msg;
+}
+ else if ($msg){
+	  
+ $msg= htmlspecialchars($msg);
+}
+
+}
+else{}
+
+
+
+?>
+
+
+<!DOCTYPE html><html lang="en">
+<head><meta charset="UTF-8"><title></title>
+</head>
+<body>
+
+<h2> <?php echo $msg; ?><br><?php echo $flag; ?> </h2>
+<h1> Easy One </h1>
+<form id="myForm">
+ 
+<input id="msg" type="text" name="msg" value="">
+<br>
+<input type="button" value="Submit" onclick="getcontent();">
+ </form>
+
+ <script type="text/javascript">
+function getcontent(){
+
+
+document.getElementById("msg").value= btoa(document.getElementById("msg").value);
+
+ 
+  document.getElementById("myForm").submit();
+ 
+}
+
+</script>
+
+</body>
+</html>
